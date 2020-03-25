e62380d7b5947036532e972302586aee5dec008b
Ankitadehkane <ankitadhekane48@gmail.com>
2020-03-25 13:46:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <input id="get" type="text"><br>
+ 
+  <button (click)="onClickMe()"> Click Me </button><br>
+  <p>{{Msg}}</p>`
+ 
 })
 export class AppComponent {
+
+  Msg ='';
+  value='';
+
+  
+  onClickMe()
+  {
+    
+    this.Msg=((document.getElementById("get")as HTMLInputElement).value);
+  }
   title = 'myapp';
 }
